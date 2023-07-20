666bfcc23d85b59a35052eaa7ad5ac6f2d370f1a
Aman Kumar Maddeshiya <aman.k.maddeshiya@zenesys.com>
2023-07-20 16:44:21 +0530

    app3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'typing';
+
+ 
+export class AppComponent implements OnInit{
+  randomText = faker.person.jobTitle(); // Rowan Nikolaus
+  enteredText = '';
+
+ 
+  ngOnInit(): void {
+   
+  }
+  onInput(event : Event){   
+    const target = event.target as HTMLInputElement;
+    const value = target.value; 
+    this.enteredText = value;
+    console.log("abc", this.enteredText);       
+  }
+
+  compare(randomLetter:string, enteredLetter:string){    
+    if(!enteredLetter){
+              return 'pending';
+    }    
+    return enteredLetter===randomLetter ? 'correct' : 'incorrect';   
+   
+  }
 }
